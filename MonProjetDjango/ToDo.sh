fef20875dc1f8bc403263f64700708191c0334a5
dondelice257 <dondelicedushime@gmail.com>
2024-11-15 13:35:56 +0100

    added todo

---
+++ MonProjetDjango/ToDo.sh
@@ -0,0 +1,12 @@
+##TODO
+
+1. Use postgres database
+2. Correct the template error
+3. Implement user authentication and authorization
+4. Add CRUD operations for managing posts
+5. Add search functionality for posts
+6. Implement pagination for posts list
+7. Implement image upload for posts
+8. Implement commenting functionality for posts
+9. Implement likes/dislikes functionality for posts
+10. Implement notifications for comments and likes
